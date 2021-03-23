30f3714caeda63bb80ad9f1cb4eaa7c700991baf
KB <katrinkajb@gmail.com>
2021-03-23 09:13:33 -0700

    cleaned up comment and added destructuring

--- fetchQuote.js
+++ fetchQuote.js
@@ -3,15 +3,13 @@ const fetch = require('node-fetch');
 const fetchQuote = async() => {
   const response = await fetch('http://futuramaapi.herokuapp.com/api/quotes/1');
 
-  const body = await response.json();
+  const [body] = await response.json();
 
-  const quote = {
+  return {
     name: body[0].character,
     text: body[0].quote,
     image: body[0].image,
   };
-
-  return quote;
 };
 
 
