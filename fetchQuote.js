d043e68030184ab274e8e6c25449adae0fd556ac
KB <katrinkajb@gmail.com>
2021-03-23 09:14:34 -0700

    fixed test

--- fetchQuote.js
+++ fetchQuote.js
@@ -3,13 +3,15 @@ const fetch = require('node-fetch');
 const fetchQuote = async() => {
   const response = await fetch('http://futuramaapi.herokuapp.com/api/quotes/1');
 
-  const [body] = await response.json();
+  const body = await response.json();
 
-  return {
+  const quote = {
     name: body[0].character,
     text: body[0].quote,
     image: body[0].image,
   };
+
+  return quote;
 };
 
 
